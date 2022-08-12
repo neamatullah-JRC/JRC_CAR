a9747d6b76391d1196ca85032739fd3fe225f008
neamatullah-JRC <73517544+neamatullah-JRC@users.noreply.github.com>
2022-08-13 01:40:51 +0600

    Create obstical_l298.ino

---
+++ obstical_l298.ino
@@ -0,0 +1,7 @@
+
+#define enA 10//Enable1 L298 Pin enA 
+#define in1 9 //Motor1  L298 Pin in1 
+#define in2 8 //Motor1  L298 Pin in1 
+#define in3 7 //Motor2  L298 Pin in1 
+#define in4 6 //Motor2  L298 Pin in1 
+#define enB 5 //Enable2 L298 Pin enB 
